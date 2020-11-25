4befaa34c4bf3d829f458763d9f6b9af3be1ac17
MaikFabricio <74901220+MaikFabricio@users.noreply.github.com>
2020-11-25 01:13:25 +0100

    Add files via upload

---
+++ Unidad 2/EjercicioIngles.java
@@ -0,0 +1,34 @@
+
+public class EjercicioIngles {
+
+	public static void main(String[] args) {
+		
+		String text01= "Cat";
+		String texto02= "Gato";
+		String texto03= "Moon";
+		String texto04= "Luna";
+		String texto05= "Car";
+		String texto06 = "Coche";
+		String texto07= "House";
+		String texto08= "casa";
+		String texto09= "River";
+		String texto10= "Rio";
+		String texto11= "Travel";
+		String texto12= "Viaje";
+		String texto13= "Peace";
+		String texto14="Paz";
+		String texto15= "headphone";
+		String texto16= "Auriculares";
+		String texto17= "Table";
+		String texto18= "Mesa";
+		String texto19= "Road";
+		String texto20= "Carretera";
+		
+		
+		
+		System.out.printf("%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n%s\t%s%n", text01,texto02,texto03, texto04, texto05, texto06, texto07, texto08, texto09,
+				texto10, texto11, texto12, texto13, texto14, texto15, texto16, texto17, texto18, texto19, 
+				texto20);
+	}
+
+}
